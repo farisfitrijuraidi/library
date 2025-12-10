4d8405842f7b23a932d4bb6d3fd422aad649b0c8
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2025-12-10 21:03:55 +0800

    Implement Book constructor and library array logic

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!")
+const myLibrary = [];
+
+function Book(title, author, year, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary (title, author, year, isRead) {
+    const book = new Book(title, author, year, isRead);
+    myLibrary.push(book);
+}
