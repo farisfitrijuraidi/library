6d7c47e5e2ee236b77e8d63b249688a6c70c841f
Faris Fitri <169568145+farisfitrijuraidi@users.noreply.github.com>
2025-12-10 23:13:46 +0800

    Create function to render library books to DOM

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('.container');
+
 const myLibrary = [];
 
 function Book(title, author, year, isRead) {
@@ -12,3 +14,28 @@ function addBookToLibrary (title, author, year, isRead) {
     const book = new Book(title, author, year, isRead);
     myLibrary.push(book);
 }
+
+function displayBook (array) {
+    container.innerHTML = '';
+    array.forEach(element => {
+        const div = document.createElement('div');
+        container.appendChild(div);
+        const title = document.createElement('p');
+        title.textContent = element.title;
+        div.appendChild(title);
+        const author = document.createElement('p');
+        author.textContent = element.author;
+        div.appendChild(author);
+        const year = document.createElement('p');
+        year.textContent = element.year;
+        div.appendChild(year);
+        const readStatus = document.createElement('p');
+        readStatus.textContent = element.readStatus;
+        div.appendChild(readStatus);
+    });
+}
+
+addBookToLibrary('The Hobbit', 'Tolkien', 1937, false);
+addBookToLibrary('1984', 'George Orwell',1949, false);
+addBookToLibrary('To Kill A Mockingbird', 'Harper Lee', 1960, true);
+displayBook(myLibrary);
